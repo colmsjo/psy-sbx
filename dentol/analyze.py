f85b1f0f20cc3750df846a5ecc85a5f9a456343f
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2018-12-29 10:24:36 +0100

    Restructure

--- dentol/analyze.py
+++ dentol/analyze.py
@@ -1,114 +1,138 @@
+#
+# analyze.py
+#
+# 181229, Jonas Colmsjö
+#
+# References: Rice, 2017, Statistics and Data Anlysis
+
+# Imports
+# ----------
+
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import pylab
 
 from os import listdir
 from math import sqrt
 from os.path import isfile, join
 from scipy import stats
 
+
+# Constants & config
+# ------------------
+
 pd.options.display.float_format = '{:,.2f}'.format
 
-PATH_DATA_FILES = './data'
+ALFAS = [0.05, 0.10, 0.20, 0.30, 0.35, 0.40]
+SKEWED = False
 SEPARATOR = ' '
-alfa05 = 0.05
-alfa10 = 0.10
-alfa20 = 0.20
-alfa30 = 0.30
-alfa35 = 0.35
-alfa40 = 0.40
+OUTLIER_LIMIT = 10000
+PATH_DATA_FILES = './data'
 
-# lists with dataframes, paths to data files and trial data (derived from the path of the data file)
-dfs = []
-files = [f for f in listdir(PATH_DATA_FILES) if isfile(join(PATH_DATA_FILES, f))]
-trials = list(map(lambda s: tuple(s.split('.')[1:4]), files))
 
-# Read the files into pandas data frames
-i = 0
-for file in files:
-    df = pd.read_csv(PATH_DATA_FILES + '/' + file, SEPARATOR,
-                     names = ['elabScenario','TASKNAME','TABLEROW','TRIALCOUNT','RT','negPos'])
-    df['datetime'] = trials[i][0]
-    df['guid'] = trials[i][2]
-    i += 1
-    df['rownum'] = np.arange(len(df))
-    dfs.append(df)
+# Read the data files
+# -------------------
 
-data = pd.concat(dfs).reset_index()
+def read_data():
+    # lists with dataframes, paths to data files and trial data (derived from the path of the data file)
+    dfs = []
+    files = [f for f in listdir(PATH_DATA_FILES) if isfile(join(PATH_DATA_FILES, f))]
+    trials = list(map(lambda s: tuple(s.split('.')[1:4]), files))
 
-# get the first data trial in the second set (row 3) and filter outliers
-rows3 = data.loc[data['rownum']==3]
-#rows3 = rows3[rows3['RT'] < 3000]
+    # Read the files into pandas data frames
+    i = 0
+    for file in files:
+        df = pd.read_csv(PATH_DATA_FILES + '/' + file, SEPARATOR,
+                         names = ['elabScenario','TASKNAME','TABLEROW','TRIALCOUNT','RT','negPos'])
+        df['datetime'] = trials[i][0]
+        df['guid'] = trials[i][2]
+        i += 1
+        df['rownum'] = np.arange(len(df))
+        dfs.append(df)
 
-print('\n----- DATA -----\n', rows3)
+    data = pd.concat(dfs).reset_index()
 
-sdata = pd.concat([rows3.groupby('elabScenario')['RT'].mean(),
-                   rows3.groupby('elabScenario')['RT'].median(),
-                   rows3.groupby('elabScenario')['RT'].var(),
-                   rows3.groupby('elabScenario')['RT'].min(),
-                   rows3.groupby('elabScenario')['RT'].max(),
-                   rows3.groupby('elabScenario')['RT'].count()],
-                   axis=1, keys=['mean', 'median', 'var', 'min', 'max', 'count'])
+    # get the first data trial in the second set (row 3) and filter outliers
+    rows3 = data.loc[data['rownum']==3]
+    rows3 = rows3[rows3['RT'] < OUTLIER_LIMIT]
 
-# MH: Mean high elab, ML: Mean low elab, MD: MH- ML, SD: Variance for MH-ML
-# SH2: Sample variance high elab, SL2: Sample variance low elab, SP: Pooled sample variance
-MH = sdata.loc[1]['mean']
-ML = sdata.loc[2]['mean']
-MD = MH - ML
-SH2 = sdata.loc[1]['var']
-SL2 = sdata.loc[2]['var']
-NumH = sdata.loc[1]['count']
-NumL = sdata.loc[2]['count']
-df = NumH+NumL-2
-SP = sqrt(((NumH-1)*SH2 +(NumL-1)*SL2) / (NumH+NumL-2))
-SD = SP*sqrt(1/NumH + 1/NumL)
-confint05 = stats.t.ppf(1-alfa05/2, df)*SP
-confint10 = stats.t.ppf(1-alfa10/2, df)*SP
-confint20 = stats.t.ppf(1-alfa20/2, df)*SP
-confint30 = stats.t.ppf(1-alfa30/2, df)*SP
-confint40 = stats.t.ppf(1-alfa40/2, df)*SP
+    return rows3
 
-print('\n----- STATS ----- (high=1 and low=2 elaboration)', )
 
-print('Means - MH:{:,.2f}, ML:{:,.2f}, MD:{:,.2f}, SD:{:,.2f}, df:{:,.2f}'.format(MH, ML, MD, SD, df))
-print('Standard errors - SH:{:,.2f}, SL:{:,.2f}, SP:{:,.2f}'.format(sqrt(SH2), sqrt(SL2), SP))
-print('Confidence interval for myH-myL is MD+-t_df(alfa05/2)*SP is ({:,.2f} , {:,.2f})'.format(MD - confint05, MD + confint05))
-print('Confidence interval for myH-myL is MD+-t_df(alfa10/2)*SP is ({:,.2f} , {:,.2f})'.format(MD - confint10, MD + confint10))
-print('Confidence interval for myH-myL is MD+-t_df(alfa20/2)*SP is ({:,.2f} , {:,.2f})'.format(MD - confint20, MD + confint20))
-print('Confidence interval for myH-myL is MD+-t_df(alfa30/2)*SP is ({:,.2f} , {:,.2f})'.format(MD - confint30, MD + confint30))
-print('Confidence interval for myH-myL is MD+-t_df(alfa40/2)*SP is ({:,.2f} , {:,.2f})'.format(MD - confint40, MD + confint40))
-print('confint05: {:,.2f}, confint10: {:,.2f}, confint20: {:,.2f}, confint30: {:,.2f}, confint40: {:,.2f}'.format(confint05, confint10, confint20, confint30, confint40))
-print('t-values for alfa/2 - t05: {:,.2f}, t10: {:,.2f}, t20: {:,.2f}, t30: {:,.2f}, t35: {:,.2f}, t40: {:,.2f}'.format(
-    stats.t.ppf(1-alfa05/2, df),
-    stats.t.ppf(1-alfa10/2, df),
-    stats.t.ppf(1-alfa20/2, df),
-    stats.t.ppf(1-alfa30/2, df),
-    stats.t.ppf(1-alfa35/2, df),
-    stats.t.ppf(1-alfa40/2, df)))
+# Calculate the statistics
+# ------------------------
 
-print('t-test statistic: {:,.6f}'.format(MD/SD))
+def calc_stats(rows3, print_stats=False):
+    sdata = pd.concat([rows3.groupby('elabScenario')['RT'].mean(),
+                       rows3.groupby('elabScenario')['RT'].median(),
+                       rows3.groupby('elabScenario')['RT'].var(),
+                       rows3.groupby('elabScenario')['RT'].min(),
+                       rows3.groupby('elabScenario')['RT'].max(),
+                       rows3.groupby('elabScenario')['RT'].count()],
+                       axis=1, keys=['mean', 'median', 'var', 'min', 'max', 'count'])
 
-print('\n-- t-test --')
-cat1 = rows3[rows3['elabScenario']==1]
-cat2 = rows3[rows3['elabScenario']==2]
+    rows3Low  = rows3[rows3['elabScenario'] == 2]
+    rows3High = rows3[rows3['elabScenario'] == 1]
 
-print(stats.ttest_ind(cat1['RT'], cat2['RT']))
+    # MH: Mean high elab, ML: Mean low elab, MD: MH- ML, SD: Variance for MH-ML
+    # SH2: Sample variance high elab, SL2: Sample variance low elab, SP: Pooled sample variance
+    MH = sdata.loc[1]['mean']
+    ML = sdata.loc[2]['mean']
+    MD = MH - ML
+    SH2 = sdata.loc[1]['var']
+    SL2 = sdata.loc[2]['var']
+    NumH = sdata.loc[1]['count']
+    NumL = sdata.loc[2]['count']
+    df = NumH+NumL-2
+    SP = sqrt(((NumH-1)*SH2 +(NumL-1)*SL2) / (NumH+NumL-2))
+    SD = SP*sqrt(1/NumH + 1/NumL)
+    confint_ = list(map(lambda x: stats.t.ppf(1-x/2, df)*SP, ALFAS))
+    confint = list(map(lambda x: (MD - x, MD + x), confint_))
+    t_values = list(map(lambda x: stats.t.ppf(1-x/2, df), ALFAS))
 
-'''
-print('\n\n**** Inverted reaction times ****')
+    if print_stats:
+        print('\n----- STATS ----- (high=1 and low=2 elaboration)', )
+        print('Means - MH:{:,.2f}, ML:{:,.2f}, MD:{:,.2f}, SD:{:,.2f}, df:{:,.2f}'.format(MH, ML, MD, SD, df))
+        print('Standard errors - SH:{:,.2f}, SL:{:,.2f}, SP:{:,.2f}'.format(sqrt(SH2), sqrt(SL2), SP))
+        print('Confidence intervals for myH-myL is MD+-t_df(alfa05/2)*SP:\n', ["(%0.2f, %0.2f)" % (x,y) for (x,y) in confint])
+        print('Calculated t-test statistic: {:,.6f}'.format(MD/SD))
+        print('t-values for alfa/2:', ["%0.2f" % i for i in t_values] )
+        print('alfas used:', ALFAS)
+        print('t-test from stats package (assumning same variance):', stats.ttest_ind(rows3High['RT'], rows3Low['RT'], equal_var=True))
+        print('t-test from stats package (assuming different variances):', stats.ttest_ind(rows3High['RT'], rows3Low['RT'], equal_var=False))
 
-#rows3['INV RT'] = rows3.apply(lambda row: 1 / row['RT'], axis=1)
-rows3.loc[:,'INV_RT'] = rows3.apply(lambda row: 1 / row['RT'], axis=1)
-#rows3.assign(INV_RT = rows3.apply(lambda row: 1 / row['RT'], axis=1))
-#print('DEBUG: res:\n', rows3)
+    return sdata
 
-print('\n-- Groupby mean / min / max --')
-print(pd.concat([rows3.groupby('elabScenario')['INV_RT'].mean(),
-                 rows3.groupby('elabScenario')['INV_RT'].min(),
-                 rows3.groupby('elabScenario')['INV_RT'].max()], axis=1))
 
-cat1 = rows3[rows3['elabScenario']==1]
-cat2 = rows3[rows3['elabScenario']==2]
+# Show the results
+# ------------------------
+# Plot with Pandas funtions:
+# https://pandas.pydata.org/pandas-docs/stable/visualization.html#visualization-kde
 
-print('\n-- t-test --')
-print(ttest_ind(cat1['INV_RT'], cat2['INV_RT']))
-'''
+def plots(rows):
+    stats.probplot(rows[rows['elabScenario'] == 2]['RT'], dist="norm", plot=pylab)
+    pylab.title('elabScenario=2')
+    pylab.show()
+    stats.probplot(rows[rows['elabScenario'] == 1]['RT'], dist="norm", plot=pylab)
+    pylab.title('elabScenario=1')
+    pylab.show()
+
+    boxplot = rows.boxplot(column=['RT'], by='elabScenario')
+    boxplot.plot()
+    plt.show()
+    rows.hist(column=['RT'], bins=10, by='elabScenario')
+    plt.show()
+
+
+# Main
+# ----
+
+rows = read_data()
+
+if SKEWED:
+    rows['RT'] = rows['RT'].apply(np.sqrt)  # np.sqrt, np.log, lambda x: 1/x etc.
+
+print('\n----- DATA -----\n', rows)
+sdata = calc_stats(rows, True)
+plots(rows)
